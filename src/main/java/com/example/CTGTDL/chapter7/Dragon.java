7d48a8720da394bb6b7de355de0822fa4f5c8d14
kenichi <baold201@gmail.com>
2024-07-31 16:54:46 +0700

    ok

---
+++ src/main/java/com/example/CTGTDL/chapter7/Dragon.java
@@ -0,0 +1,8 @@
+package com.example.CTGTDL.chapter7;
+
+//public class Dragon extends Monster {
+//    public boolean frighten(int degree) {
+//        System.out.println("breath fire");
+//        return true;
+//    }
+//}
